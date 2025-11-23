a121565e6854c9bf0e939974e3227eacc42961f3
IntiMG <intimontalvan@uamv.edu.ni>
2025-11-22 18:40:11 -0600

    Se agrega stock minimo y funcion para saber si esta o no bajo de stock

--- FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/Producto.java
+++ FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/Producto.java
@@ -9,7 +9,7 @@ import lombok.*;
 @Entity @Getter @Setter
 public class Producto {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Hidden
     private int idProducto;
 
     @Column(length=50) @Required
@@ -26,6 +26,28 @@ public class Producto {
     @Min(0)
     private int stock;
 
+    @Min(0)
+    @Column(nullable=false)
+    private int stockMinimo = 0; // Para alertas de bajo stock
+
     @Column(length=20)
     private String unidadMedida;
+
+    public void aumentarStock(int cantidad){
+        if(cantidad <= 0) throw new IllegalArgumentException("El cantidad debe ser mayor a 0");
+        this.stock += cantidad;
+    }
+
+    public void disminuirStock(int cantidad){
+        if(cantidad <= 0) throw new IllegalArgumentException("El cantidad debe ser mayor a 0");
+        if(stock - cantidad < 0) {
+            throw new IllegalArgumentException("Stock insuficiente para la operacion");
+        }
+        this.stock -= cantidad;
+    }
+
+    @Depends("stock, stockMinimo")
+    public boolean isBajoStock() {
+        return stockMinimo > 0 && stock <= stockMinimo;
+    }
 }
